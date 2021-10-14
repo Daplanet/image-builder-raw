6d06ef899b22f969a23a4754b72a9c66c3faae05
Dwight Spencer <dspencer@computekindustries.com>
2021-10-14 07:48:23 -0500

    Create build_test.sh
    Changes:
      - Migrates rspec to BATS

---
+++ builder/rpi/build_test.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bats
+## Raw Image
+declare -r image_path="/rpi-raw.img"
+
+@test "Image should exist" {
+  [ -f $image_path ]
+}
+
+declare -r context="Partition Table"
+
+@test "${context} should have 2 partitions" {
+  run "fdisk -l ${image_path} | grep '^/rpi-raw'"
+  [ "$status" -eq 0 ]
+  [ "${lines[@]}" -eq 2 ]
+}
+
+@test "${context} should have a boot-partition with a sda1 W95 FAT23 filesystem" {
+  match='^.*\.img1 \* .*W95 FAT32 \(LBA\)'
+  run "fdisk -l ${image_path} | grep '^/rpi-raw'"
+  [ "$output" =~ $match ]
+}
+
+@test "${context} should have a root-partition with SDA2 Linux filesystem" {
+  match='^.*\.img2 .*Linux$'
+  run "fdisk -l ${image_path} | grep '^/rpi-raw'"
+  [ "$output" =~ $match ]
+}
+
+@test "${context} for partion sda1 starts at sector 2048" {
+  match='^.*\.img1\ \* *2048 .*$'
+  run "fdisk -l ${image_path} | grep '^/rpi-raw'"
+  [ "$output" =~ $match ]
+}
+
+@test "${context} for partion sda1 is size of 100M" {
+  match='^.*\.img1.* 100M  c.*$'
+  run "fdisk -l ${image_path} | grep '^/rpi-raw'"
+  [ "$output" =~ $match ]
+}
